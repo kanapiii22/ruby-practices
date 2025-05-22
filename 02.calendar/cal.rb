a608900f4a4fccdeb76203ce3adcec833dd374e3
kanapiii22 <205907063+kanapiii22@users.noreply.github.com>
2025-05-22 16:58:11 +0900

    eachメソッド内の変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -9,19 +9,19 @@ require 'date'
 opt = OptionParser.new
 opt.on('-m MONTH', Integer) { |m| @month = m }
 opt.on('-y YEAR', Integer) { |y| @year = y }
-opt.parse(ARGV) 
+opt.parse(ARGV)
 
 today = Date.today
 year = @year || today.year
 month = @month || today.month
 
 if year < 1970 || year > 2100
-  puts "対応している年は1970年から2100年までです"
+  puts '対応している年は1970年から2100年までです'
   exit
 end
 
 if month < 1 || month > 12
-  puts "月は1〜12の範囲で指定してください"
+  puts '月は1〜12の範囲で指定してください'
   exit
 end
 
@@ -31,17 +31,15 @@ end_date = Date.new(year, month, -1)
 
 header = "#{year}年#{month}月"
 puts header.center(20)
-puts "日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 
 # 1日の曜日（0=日, 6=土）に応じてスペースを出力
-print "   " * start_date.wday
+print '   ' * start_date.wday
 
 # 日付を1日ずつ表示（右揃え2桁 + 半角スペース）
 (start_date..end_date).each do |date|
-  print date.day.to_s.rjust(2) + " "
+  print date.day.to_s.rjust(2) + ' '
   puts if date.saturday?
-  date += 1
 end
 
-puts 
-
+puts
