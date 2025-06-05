0ad5afb8dd9ff22253621780c2370e5156ab6ed1
kanapiii22 <205907063+kanapiii22@users.noreply.github.com>
2025-06-05 13:07:25 +0900

    重複をなくし、メソッドを追加

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+FRAME_COUNT = 10
 def parse_input(input)
   input.split(',').map { |s| s == 'X' ? 10 : s.to_i }
 end
@@ -9,7 +10,7 @@ def make_frames(shots)
   frames = []
   i = 0
 
-  9.times do
+  (FRAME_COUNT - 1).times do
     if shots[i] == 10
       frames << [10]
       i += 1
@@ -25,28 +26,24 @@ end
 # スコア計算
 def calculate_score(frames)
   total = 0
-  frame_count = 10
   frames.each_with_index do |frame, idx|
-    if idx < frame_count - 1 
-      if frame[0] == 10 # ストライク
-        total += 10 + strike_bonus(frames, idx)
-      elsif frame.sum == 10 # スペア
-        total += 10 + spare_bonus(frames, idx)
-      else
-        total += frame.sum
-      end
-    else
-      total += frame.sum
-    end
+    total += frame.sum
+    total += bonus_for(frame, frames, idx) if idx < FRAME_COUNT - 1
   end
+  total
+end
 
-  puts total
+def bonus_for(frame, frames, idx)
+  return strike_bonus(frames, idx) if strike?(frame)
+  return spare_bonus(frames, idx) if spare?(frame)
+
+  0
 end
 
 def strike_bonus(frames, idx)
   next_frame = frames[idx + 1]
 
-  if next_frame[0] == 10 && frames[idx + 2]
+  if strike?(next_frame) && frames[idx + 2]
     10 + frames[idx + 2][0]
   else
     next_frame[0] + (next_frame[1] || 0)
@@ -57,7 +54,14 @@ def spare_bonus(frames, idx)
   frames[idx + 1][0]
 end
 
+def strike?(frame)
+  frame[0] == 10
+end
+
+def spare?(frame)
+  frame.size == 2 && frame.sum == 10
+end
+
 shots = parse_input(ARGV[0])
 frames = make_frames(shots)
-score = calculate_score(frames)
-
+puts calculate_score(frames)
