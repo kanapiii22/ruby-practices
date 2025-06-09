112bfb75e4d856d67a07f613b3a465d554be40d8
kanapiii22 <205907063+kanapiii22@users.noreply.github.com>
2025-06-09 15:40:43 +0900

    ディレクトリのファイルを取得してソートするメソッド追加

--- 04.ls/my_ls.rb
+++ 04.ls/my_ls.rb
@@ -1,4 +1,8 @@
 # 引数を取得（なければカレントディレクトリ）
 target = ARGV[0] || "."
 
-puts "対象ディレクトリ: #{target}"
+# 引数で指定されたディレクトリの内容を取得し、ソートして表示する
+def collect_entries(target)
+  Dir.entries(target).reject { |entry| entry.start_with?('.') }.sort
+end
+puts collect_entries(target)
