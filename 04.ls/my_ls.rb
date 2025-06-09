d56963458bb38de57ee8b741dafc9810c76e5b53
kanapiii22 <205907063+kanapiii22@users.noreply.github.com>
2025-06-09 16:45:10 +0900

    テストファイルを追加、配列を３列に変換

--- 04.ls/my_ls.rb
+++ 04.ls/my_ls.rb
@@ -1,8 +1,22 @@
 # 引数を取得（なければカレントディレクトリ）
 target = ARGV[0] || "."
 
+FORMAT_COL = 3
+
 # 引数で指定されたディレクトリの内容を取得し、ソートして表示する
 def collect_entries(target)
   Dir.entries(target).reject { |entry| entry.start_with?('.') }.sort
 end
-puts collect_entries(target)
+p collect =  collect_entries(target)
+
+# 配列を3列に変換
+def format_rows(collect, column_count)
+  columns = Array.new(column_count) { [] }
+
+  collect.each_with_index do |entry, index|
+  col = index % column_count
+  columns[col] << entry  # 各列に順番に詰める
+  end
+  columns
+end
+p format = format_rows(collect,FORMAT_COL)
